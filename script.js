6494b73e2e39790e623e0e811f67a40b9fc22ac9
Mahiballah <145888808+mahibullah11@users.noreply.github.com>
2024-04-28 22:05:16 +0500

    done with dice

---
+++ script.js
@@ -0,0 +1,9 @@
+function rollDice(){
+    let scoreEle =  document.getElementById('score');
+    let diceimg = document.getElementById('diceImg');
+    let score = Math.ceil(Math.random() * 6);
+    
+    scoreEle.innerText = score;
+  
+    diceimg.src = `dice-${score}.jpeg`;
+}  
